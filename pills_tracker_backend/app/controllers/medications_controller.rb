4c73ffbaca53ac461fbcfcc358cc00c8e4714176
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:30:35 -0700

    Update medications_controller.rb
    Full changes to render meds routes

--- pills_tracker_backend/app/controllers/medications_controller.rb
+++ pills_tracker_backend/app/controllers/medications_controller.rb
@@ -1,2 +1,32 @@
 class MedicationsController < ApplicationController
+  def index
+    medications = Medication.all
+    render json: MedicationSerializer.new(medications)
+  end
+
+  def show
+    medication = Medication.find(params[:id])
+    render json: MedicationSerializer.new(medication)
+  end
+
+  def create
+    medication = Medication.new(m_params)
+
+    render json: medication.save
+  end
+
+  def update
+    @medication = Medication.find(params[:id])
+    @medication.update(name: params["medication"]["name"])
+    @medication.update(priceperpill: params["medication"]["priceperpill"])
+    @medication.save
+    render json: @medication
+  end
+
+  private
+
+  def m_params
+      params.permit(:name, :priceperpill)
+  end
+
 end
